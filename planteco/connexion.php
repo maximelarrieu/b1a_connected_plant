bd1bc85187edf14750dcee17a799c0048968479c
Maxime Larrieu <43401890+maximelarrieu@users.noreply.github.com>
2019-06-11 10:26:25 +0200

    update connexion.php

--- planteco/connexion.php
+++ planteco/connexion.php
@@ -2,7 +2,7 @@
   $server = "localhost";
   $db = "planteco";
   $login = "root";
-  $password = "";
+  $password = "root";
   $connexion = new PDO("mysql:host=$server;dbname=$db", $login, $password);
   $connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
